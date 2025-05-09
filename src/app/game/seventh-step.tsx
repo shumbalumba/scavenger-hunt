b4dfb6ed06d6d528710f8919d1e2e0325fe574f0
Agnis <agnis@Agniss-MacBook-Pro.local>
2025-05-09 17:39:38 +0300

    last step

--- src/app/game/seventh-step.tsx
+++ src/app/game/seventh-step.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import { CoolText } from "../components/cool-text";
+import { Form } from "../components/form";
 
 const texts = ["GAL ZEMELAPIS PARODYS KELIA?"];
 
 export const SeventhStep = () => {
+  const [hasAnswer, setHasAnswer] = useState(false);
+
   return (
     <>
-      <CoolText texts={texts} capitalize={true} color="green" />
+      <CoolText texts={texts} capitalize={true} />
+      <Form
+        answer="FINISOTIESIOJI"
+        onSuccess={() => setHasAnswer(true)}
+        placeholder="MAMMA MIA"
+      />
+      {hasAnswer && <CoolText texts={["LEK KUR PARASYTA!"]} />}
     </>
   );
 };
