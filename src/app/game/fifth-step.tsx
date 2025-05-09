1a54e6f0a97b93503df3a6d77ad3709c8b6174aa
Agnis <agnis@Agniss-MacBook-Pro.local>
2025-05-09 18:07:58 +0300

    viskas gal?

--- src/app/game/fifth-step.tsx
+++ src/app/game/fifth-step.tsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 import { CoolText } from "../components/cool-text";
 import { Form } from "../components/form";
 
-const texts = ["Jūsų siunta paštomate"];
+const texts = [
+  "OMNIVA:",
+  "Siunta CF098800731EE jau Vilniaus NORFA Zvalgu pastomatas. Kodas: 734899. Saugosime iki 2025.05.16.",
+];
 
 export const FifthStep = ({ cb }: { cb: () => void }) => {
   const [showPlayer, setShowPlayer] = useState(false);
